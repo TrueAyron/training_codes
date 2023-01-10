941844bc3ec56fee307c69f438ece7c7b85eb1ec
Ayron <ayronlouzada19@gmail.com>
2023-01-10 15:27:10 -0300

    Update tictactoe.py
    Restart botton

--- tictactoe.py
+++ tictactoe.py
@@ -1,47 +1,74 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class TicTacToe:
-    def __init__(self):
-        self.root = tk.Tk()
+    def __init__(self, root):
+        self.root = root
         self.root.title("Jogo da Velha")
-
-        self.buttons = []
-        for i in range(3):
-            self.buttons.append([])
-            for j in range(3):
-                button = tk.Button(self.root, width=10, height=5, command=lambda i=i, j=j: self.play(i, j))
-                button.grid(row=i, column=j)
-                self.buttons[i].append(button)
-
-        self.player = "X"
-        self.root.mainloop()
-
-    def play(self, i, j):
-        button = self.buttons[i][j]
-        if button["text"] == "":
-            button["text"] = self.player
-            self.check_win()
-            self.player = "O" if self.player == "X" else "X"
-
-    def check_win(self):
-        for i in range(3):
-            if self.buttons[i][0]["text"] == self.buttons[i][1]["text"] == self.buttons[i][2]["text"] != "":
-                self.end_game(self.buttons[i][0]["text"] + " ganhou!")
-            elif self.buttons[0][i]["text"] == self.buttons[1][i]["text"] == self.buttons[2][i]["text"] != "":
-                self.end_game(self.buttons[0][i]["text"] + " ganhou!")
         
-        if self.buttons[0][0]["text"] == self.buttons[1][1]["text"] == self.buttons[2][2]["text"] != "":
-            self.end_game(self.buttons[0][0]["text"] + " ganhou!")
-        elif self.buttons[0][2]["text"] == self.buttons[1][1]["text"] == self.buttons[2][0]["text"] != "":
-            self.end_game(self.buttons[0][2]["text"] + " ganhou!")
-
-    def end_game(self, message):
-        for i in range(3):
-            for j in range(3):
-                self.buttons[i][j]["state"] = "disabled"
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.player = 'X'
         
-        tk.Label(self.root, text=message).grid(row=3, column=0, columnspan=3)
-
-
-if __name__ == "__main__":
-    TicTacToe()
+        self.create_widgets()
+        
+    def create_widgets(self):
+        self.buttons = [[tk.Button(self.root, width=5, height=2, command=lambda r=r, c=c: self.play(r, c)) for c in range(3)] for r in range(3)]
+        
+        for r in range(3):
+            for c in range(3):
+                self.buttons[r][c].grid(row=r, column=c)
+                
+        self.restart_button = tk.Button(self.root, text="Reiniciar", command=self.restart)
+        self.restart_button.grid(row=3, column=1)
+        
+    def play(self, row, col):
+        if self.board[row][col] != ' ':
+            return
+        
+        self.buttons[row][col]["text"] = self.player
+        self.board[row][col] = self.player
+        
+        winner = self.check_winner()
+        if winner:
+            self.show_winner(winner)
+        elif self.is_full():
+            self.show_winner('empate')
+        else:
+            self.player = 'O' if self.player == 'X' else 'X'
+    
+    def check_winner(self):
+        lines = (
+            self.board[0],  # first row
+            self.board[1],  # second row
+            self.board[2],  # third row
+            [self.board[0][0], self.board[1][0], self.board[2][0]],  # first col
+            [self.board[0][1], self.board[1][1], self.board[2][1]],  # second col
+            [self.board[0][2], self.board[1][2], self.board[2][2]],  # third col
+            [self.board[0][0], self.board[1][1], self.board[2][2]],  # diagonal
+            [self.board[0][2], self.board[1][1], self.board[2][0]],  # diagonal
+        )
+        if any(all(cell == self.player for cell in line) for line in lines):
+            return self.player
+        return None
+    
+    def is_full(self):
+        return all(all(cell != ' ' for cell in row) for row in self.board)
+    
+    def show_winner(self, winner):
+        if winner == 'empate':
+            message = "Empate!"
+        else:
+            message = winner + " ganhou!"
+        messagebox.showinfo("Fim de Jogo", message)
+    
+    def restart(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.player = 'X'
+        for r in range(3):
+            for c in range(3):
+                self.buttons[r][c]["text"] = ' '
+                
+if __name__ == '__main__':
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
