aa86b656cd1818973404f92b3bbf3ebd3714ee2e
Ayron <ayronlouzada19@gmail.com>
2023-01-11 17:01:52 -0300

    Update avltree.py
    remove function

--- avltree.py
+++ avltree.py
@@ -82,3 +82,59 @@ class AVL_Tree:
             return 0
 
         return self.getHeight(root.left) - self.getHeight(root.right)
+
+    def remove(self, root, key):
+
+        if not root:
+            return root
+
+        elif key < root.key:
+            root.left = self.remove(root.left, key)
+
+        elif key > root.key:
+            root.right = self.remove(root.right, key)
+
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = self.getMinValueNode(root.right)
+            root.key = temp.key
+            root.right = self.remove(root.right,
+                                      temp.key)
+
+        if root is None:
+            return root
+
+        root.height = 1 + max(self.getHeight(root.left),
+                              self.getHeight(root.right))
+
+        balance = self.getBalance(root)
+
+        if balance > 1 and self.getBalance(root.left) >= 0:
+            return self.rightRotate(root)
+
+        if balance < -1 and self.getBalance(root.right) <= 0:
+            return self.leftRotate(root)
+
+        if balance > 1 and self.getBalance(root.left) < 0:
+            root.left = self.leftRotate(root.left)
+            return self.rightRotate(root)
+
+        if balance < -1 and self.getBalance(root.right) > 0:
+            root.right = self.rightRotate(root.right)
+            return self.leftRotate(root)
+
+        return root
+        
+    def getMinValueNode(self, root):
+        if root is None or root.left is None:
+            return root
+        return self.getMinValueNode(root.left)
