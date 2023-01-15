b1605a6d97d291b2b88c0e5240515a930f5be4ed
Ayron <ayronlouzada19@gmail.com>
2023-01-15 18:08:22 -0300

    Update roman_to_decimal.py
    Add the decimal to roman

--- roman_to_decimal.py
+++ roman_to_decimal.py
@@ -8,5 +8,21 @@ def roman_to_decimal(rom):
             dec_val += rom_val[rom[i]]
     return dec_val
 
+def decimal_to_roman(dec):
+    dec_val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    rom_val = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    roman_num = ""
+    i = 0
+    while dec > 0:
+        for _ in range(dec // dec_val[i]):
+            roman_num += rom_val[i]
+            dec -= dec_val[i]
+        i += 1
+    return roman_num
+
+print(decimal_to_roman(14)) # output XIV
+print(decimal_to_roman(21)) # output XXI
+
+
 print(roman_to_decimal("XIV")) # output 14
 print(roman_to_decimal("XXI")) # output 21
