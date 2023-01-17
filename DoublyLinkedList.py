cb99de296f8c20f41f2c4628ac1e74fe386940de
Ayron <86243151+TrueAyron@users.noreply.github.com>
2023-01-17 12:40:59 -0300

    Create DoublyLinkedList.py

---
+++ DoublyLinkedList.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None 
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
+        new_node.prev = curr
+    
+    def print_list(self):
+        curr = self.head
+        while curr:
+            print(curr.data)
+            curr = curr.next
